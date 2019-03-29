200be70965f8d4e38f7e4e2bbacd809a2995e421
Anweshan Mukherjee <anweshanmukherjee22@gmail.com>
2019-03-30 00:33:30 +0530

    Done

---
+++ Assignment1/Factorial.java
@@ -0,0 +1,9 @@
+class Factorial{
+  public static void main(String[] args) {
+    int num = 3, fact = 1, dup = num;
+    while(dup!=0){
+      fact*=dup--;
+    }
+    System.out.println(num+"! = "+fact);
+  }
+}
