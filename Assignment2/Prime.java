74442e27cd7f302c20ac69497db827abbfee6bb8
Anweshan Mukherjee <anweshanmukherjee22@gmail.com>
2019-03-31 01:09:04 +0530

    Question2

---
+++ Assignment2/Prime.java
@@ -0,0 +1,19 @@
+//Question2
+class Prime{
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        if(num==1)
+            System.out.println("Unique number");
+        else if(ifPrime(num))
+            System.out.println("Prime number");
+        else
+            System.out.println("Composite number");
+    }
+    public static boolean ifPrime(int num){
+        for(int i=2;i<=num/i;i++) {
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
+}
