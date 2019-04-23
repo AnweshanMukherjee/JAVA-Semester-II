619199b4811043beda081e0c11952aab07d55544
Anweshan Mukherjee <anweshanmukherjee22@gmail.com>
2019-04-24 00:54:45 +0530

    Question 2

--- Assignment4/MyString.java
+++ Assignment4/MyString.java
@@ -4,13 +4,43 @@ class MyString{
     private int length;
     MyString(){
         str = new char[80];
-        length = 80;
         str[0] = '0';
+        length = 80;
     }
-    MyString(char str1[]){
-        str = new char[str1.length];
-        length = str1.length;
-        for(int i=0;i<str1.length;i++)
-            str[i] = str1[i];
+    MyString(char s[]){
+        str = new char[s.length];
+        for(int i=0;i<s.length;i++)
+            str[i] = s[i];
+        length = s.length;
+    }
+    MyString(MyString obj){
+        this(obj.str);
+    }
+    boolean myStartsWith(char ar[]){
+        for(int i=0;i<ar.length;i++)
+            if(str[i]!=ar[i])
+                return false;
+        return true;
+    }
+    boolean myEndsWith(char ar[]){
+        int pos = 1;
+        for(int i=ar.length-1;i>=0;i--){
+            if(ar[i]!=str[length-pos])
+                return false;
+            pos++;
+        }
+        return true;
+    }
+    char myCharAt(int pos){
+        return str[pos];
+    }
+    public static void main(String[] args) {
+        char s1[] = {'a', 'b', 'c', '1'};
+        char s2[] = {'a', 'b'};
+        char s3[] = {'c', '1'};
+        MyString obj = new MyString(new MyString(s1));
+        System.out.println(obj.myEndsWith(s2));
+        System.out.println(obj.myEndsWith(s3));
+        System.out.println(obj.myCharAt(2));
     }
 }
